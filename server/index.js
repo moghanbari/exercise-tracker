cd74e260164ec15e805491703664abad2e95c95f
Mo Ghanbari <mostafa.qanbary@gmail.com>
2020-05-29 10:47:51 -0400

    Setup .env file. Changed .env file default address. Added .env file to .gitignore. Setup the connection with MongoDB using Mongoose

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
-require('dotenv').config()
+require('dotenv').config({ path: path.join(__dirname, '.env') })
 
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+})
+const connection = mongoose.connection
+connection.once('open', () => {
+  console.log('MongoDB connection was successful')
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
